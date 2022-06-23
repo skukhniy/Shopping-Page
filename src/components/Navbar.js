b6a43757c06978115b37fcdda08e9bc5ecd7f084
Stan <skukhniy@gmail.com>
2022-06-22 23:28:48 -0400

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div id="Navbar">
+      <h2>Stanley's Records</h2>
+      <ul>
+        <li><a href="/shop">Home</a></li>
+        <li><a href="/shop">Shop</a></li>
+      </ul>
+    </div>
+  )
+}
