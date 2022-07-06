5f73244208720b0d610465f8dda76639a0ab045b
Stan <skukhniy@gmail.com>
2022-07-06 12:31:34 -0400

    added gh page deployment

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
       <BrowserRouter>
         <Navbar cartArray={cartArray} />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
           {albumRoutes}
           <Route path="/cart" element={<Cart cartArray={cartArray} setCart={setCart} subtotalState={subtotalState} setSubtotal={setSubtotal} />} />
